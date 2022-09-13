8d5b232cd534bcbc256262cebbb5382a600166ad
Jorge Ramón <jorge.ramon2128@gmail.com>
2022-09-13 16:37:13 -0500

    Added FizzBuzz solution

--- 00-recap/fizzbuzz.js
+++ 00-recap/fizzbuzz.js
@@ -25,7 +25,23 @@
  */
 
 function fizzbuzz() {
-  //...
+  for (let i = 0; i <= 100; i++) {
+    let result = "";
+
+    if (i%3 === 0 && i !== 0) {
+      result = result + "Fizz";
+    }
+
+    if (i%5 === 0 && i !== 0) {
+      result += "Buzz";
+    }
+
+    if (result === "") {
+      result = i;
+    }
+
+    console.log(result);
+  }
 }
 
 fizzbuzz();
