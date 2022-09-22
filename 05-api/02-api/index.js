46e5a616eccc67707565fc840bcd245a55966781
Jorge Ramón <jorge.ramon2128@gmail.com>
2022-09-22 17:09:06 -0500

    Finished example 02

--- 05-api/02-api/index.js
+++ 05-api/02-api/index.js
@@ -1,11 +1,14 @@
+// Consultamos la lista de todas las razas
 fetch('https://bedu-dogs-api.herokuapp.com/breeds')
   .then(function (response) {
     return response.json();
   })
   .then(function ({ data }) {
+    // Actualizamos el <select> con las razas
     updateSelect(data);
   });
 
+// Renderiza una lista de razas dentro del <select>
 function updateSelect(breeds) {
   const select = document.getElementById('breeds');
 
@@ -21,4 +24,28 @@ function updateSelect(breeds) {
 
     select.append(option);
   }
+}
+
+const button = document.getElementById('get-image');
+
+// Escuchando un evento para descargar una imagen
+button.addEventListener('click', function () {
+  // Obtengo la raza seleccionada
+  const select = document.getElementById('breeds');
+
+  // Consulto la imagen aleatoria de la raza seleccionada
+  fetch(`https://bedu-dogs-api.herokuapp.com/breeds/${select.value}/images`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function ({data}) {
+      // Actualizar la imagen en el HTML
+      updateImg(data);
+    });
+});
+
+// Toma una url e inserta una etiqueta <img> en el HTML
+function updateImg(url) {
+  const container = document.getElementById('img-container');
+  container.innerHTML = `<img src="${url}" class="img-fluid"/>`;
 }
