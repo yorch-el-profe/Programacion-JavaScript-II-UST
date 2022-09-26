4fc135fbcabe975c50ea61780358838827e86a9e
Jorge Ramón <jorge.ramon2128@gmail.com>
2022-09-26 15:57:49 -0500

    Added example of Babel + Webpack

---
+++ 07-babel/01-babel-webpack/webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  entry: 'index.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        // Soporte extensiones .mjs y .js
+        test: /\.m?js$/,
+        // No tome en cuenta la carpeta node_modules
+        exclude: /node_modules/,
+        // Configuración de Babel
+        use: {
+          loader: 'babel-loader',
+          options: {
+            // Incluya el plugin llamado @babel/preset-env
+            // Permite utilizar las características de ES2015+
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+}
