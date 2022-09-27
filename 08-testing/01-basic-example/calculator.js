708fffab7c070279f75440a4ac51fa49808cc75b
Jorge Ramón <jorge.ramon2128@gmail.com>
2022-09-27 16:16:28 -0500

    Completed example of sum function

--- 08-testing/01-basic-example/calculator.js
+++ 08-testing/01-basic-example/calculator.js
@@ -3,9 +3,9 @@
 
 // CommonJS
 exports.sum = function (a, b) {
+  if (typeof a !== "number" || typeof b !== "number") {
+    return NaN;
+  }
+  
   return a + b;
-}
-
-exports.subs = function (a, b) {
-  return a - b;
 }
--- 08-testing/01-basic-example/calculator.test.js
+++ 08-testing/01-basic-example/calculator.test.js
@@ -8,10 +8,33 @@ const { sum } = require('./calculator');
 describe('Testing sum function', () => {
   test('0 + 0 = 0', () => {
     // Esperaría que sum(0,0) sea igual a 0
+    // toBe son valores exactos
     expect(sum(0,0)).toBe(0);
   });
-});
 
-describe('Testing subs function', () => {
+  test('n + 0 = n', () => {
+    const random = Math.ceil(Math.random() * 1000000);
+    expect(sum(random, 0)).toBe(random)
+  });
 
+  test('0 + m = m', () => {
+    const random = Math.ceil(Math.random() * 1000000);
+    expect(sum(0, random)).toBe(random);
+  });
+
+  test('negative numbers', () => {
+    const random1 = -1 * Math.ceil(Math.random() * 1000000);
+    const random2 = -1 * Math.ceil(Math.random() * 1000000);
+    expect(sum(random1, random2)).toBeLessThan(0);
+  });
+
+  test('strings', () => {
+    expect(sum("0", "1")).toBeNaN();
+  })
+
+  test('any other type', () => {
+    expect(sum(true, {})).toBeNaN();
+    expect(sum([], null)).toBeNaN();
+    expect(sum()).toBeNaN();
+  });
 });
